de5b2c490ce5db4de3818d311b91335efd1b04f9
thomas <thomas.cornet@me.com>
2019-03-25 17:08:56 +0100

    UI changes and added a TimerState for improved logic control

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 
+enum TimerState { init, play, pause, reset, end }
+
 void main() => runApp(App());
 
 const black = Colors.black87;
@@ -24,29 +26,42 @@ class Page extends StatefulWidget {
 }
 
 class _PageState extends State<Page> {
+  TimerState _state = TimerState.init;
+
   double _h = 0;
   int _limit = 120;
   int _sec = 0;
   double _op = 0;
   double _tick = 1;
-  var _dur = Duration(milliseconds: 350);
+  var _d = Duration(milliseconds: 350);
   Stopwatch _watch = Stopwatch();
   Color _c = black;
-  TextStyle _tts = TextStyle(
+  TextStyle _ts = TextStyle(
     fontSize: 38,
     color: black,
   );
-  TextStyle _cts = TextStyle(
+  TextStyle _cs = TextStyle(
     fontSize: 16,
     color: black,
   );
 
+  bool hasStarted = false;
+
+  String _actionLabel = "Go";
+
   void update(Timer t) {
-    if (_watch.isRunning) {
+    print("update");
+    if (_state == TimerState.play) {
       setState(() {
         _sec = _watch.elapsed.inSeconds;
         _tick = 1 - _tick;
-        _h = (_sec / _limit) * MediaQuery.of(context).size.height * 0.6;
+
+        Curve curve = Curves.easeOutQuart;
+
+        _h = curve.transform(_sec / _limit) *
+            MediaQuery.of(context).size.height *
+            0.6;
+
         if (_sec == _limit) {
           _watch.stop();
           _h = MediaQuery.of(context).size.height;
@@ -55,18 +70,60 @@ class _PageState extends State<Page> {
           t.cancel();
         }
       });
-    } else {
-      _tick = 1 - _tick;
+    }
+
+    if (_state == TimerState.reset) {
       t.cancel();
     }
   }
 
+  void start() {
+    print("Start");
+    setState(() {
+      _watch.start();
+      Timer.periodic(Duration(milliseconds: 950), update);
+      hasStarted = true;
+      _state = TimerState.play;
+      _actionLabel = "Pause";
+    });
+  }
+
+  void pause() {
+    print("Pause");
+    setState(() {
+      _watch.stop();
+      _actionLabel = "Go";
+      _state = TimerState.pause;
+    });
+  }
+
+  void resume() {
+    print("Resume");
+    setState(() {
+      _watch.start();
+      _actionLabel = "Pause";
+      _state = TimerState.play;
+    });
+  }
+
+  void end() {
+    print("End");
+    setState(() {
+      _watch.stop();
+      _actionLabel = "Reset";
+      _state = TimerState.reset;
+    });
+  }
+
   void reset() {
+    print("Reset");
     setState(() {
       _sec = 0;
       _h = _op = 0;
       _c = black;
       _watch.reset();
+      hasStarted = false;
+      _state = TimerState.reset;
     });
   }
 
@@ -75,116 +132,195 @@ class _PageState extends State<Page> {
     return Scaffold(
       body: Stack(
         children: [
+          !hasStarted
+              ? Positioned(
+                  top: MediaQuery.of(context).size.height * 0.7,
+                  height: 250 * (1 - _op),
+                  width: MediaQuery.of(context).size.width,
+                  child: Center(
+                    child: Padding(
+                      padding: EdgeInsets.all(8),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Reclaim your workday!",
+                            style: _ts,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Container(
+                                height: 45,
+                                child: FloatingActionButton(
+                                  foregroundColor: Colors.white,
+                                  backgroundColor: black,
+                                  child: Text("Less"),
+                                  onPressed: () {
+                                    setState(() {
+                                      _limit =
+                                          _limit - 30 < 0 ? 0 : _limit - 30;
+                                    });
+                                  },
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  "${(_limit / 60).floor().toString()} min. limit",
+                                ),
+                              ),
+                              Container(
+                                height: 45,
+                                child: FloatingActionButton(
+                                  foregroundColor: Colors.white,
+                                  backgroundColor: black,
+                                  child: Text("More"),
+                                  onPressed: () {
+                                    setState(() {
+                                      _limit =
+                                          _limit + 30 > 300 ? 300 : _limit + 30;
+                                    });
+                                  },
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+              : Container(),
+          Positioned(
+            top: MediaQuery.of(context).size.height * (1 - 0.6),
+            height: 2 * (1 - _op),
+            width: MediaQuery.of(context).size.width,
+            child: Divider(
+              height: 2,
+              color: black,
+            ),
+          ),
           Align(
             alignment: Alignment.bottomCenter,
             child: AnimatedContainer(
-              duration: _dur,
-              curve: curve,
+              duration: Duration(milliseconds: 700),
+              curve: Curves.easeOutBack,
               color: _c,
               height: _h,
             ),
           ),
-          Padding(
-            padding: EdgeInsets.all(60),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.all(24),
-                  child: Row(
+          hasStarted
+              ? Padding(
+                  padding: EdgeInsets.all(60),
+                  child: Column(
                     crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      AnimatedOpacity(
-                        child: Text(
-                          "TICK",
-                          style: _tts,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(24),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            AnimatedOpacity(
+                              child: Text(
+                                "TICK",
+                                style: _ts,
+                              ),
+                              opacity: _tick - _op,
+                              duration: _d,
+                              curve: curve,
+                            ),
+                            AnimatedOpacity(
+                              child: Text(
+                                "TOCK",
+                                style: _ts,
+                              ),
+                              opacity: 1.0 - _tick,
+                              duration: _d,
+                              curve: curve,
+                            ),
+                          ],
                         ),
-                        opacity: _tick - _op,
-                        duration: _dur,
-                        curve: curve,
                       ),
-                      AnimatedOpacity(
-                        child: Text(
-                          "TOCK",
-                          style: _tts,
-                        ),
-                        opacity: 1.0 - _tick,
-                        duration: _dur,
-                        curve: curve,
+                      Text("Time left:"),
+                      Text(
+                        "${((_limit - _sec) / 60).floor().toString().padLeft(2, "0")} min. ${((_limit - _sec) % 60).toString().padLeft(2, "0")} sec.",
+                        style: _cs,
                       ),
                     ],
                   ),
-                ),
-                Text(
-                  "${(_sec / 60).floor().toString().padLeft(2, "0")} min. ${(_sec % 60).toString().padLeft(2, "0")} sec.",
-                  style: _cts,
-                ),
-              ],
-            ),
-          ),
+                )
+              : Container(),
           Align(
             alignment: Alignment.center,
             child: AnimatedOpacity(
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text("WASTED", style: _tts),
+                  Text("WASTED", style: _ts),
                   Text(
                     "Try again!",
-                    style: _cts,
+                    style: _cs,
                   ),
                 ],
               ),
               opacity: _op,
-              duration: _dur,
+              duration: _d,
               curve: curve,
             ),
           ),
-          Positioned(
-            top: MediaQuery.of(context).size.height * (1 - 0.6),
-            height: 60 * (1 - _op),
-            width: MediaQuery.of(context).size.width,
-            child: Column(
-              children: [
-                Divider(
-                  height: 2,
-                  color: black,
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8),
-                  child: Text(
-                      "Reclaim your workday! ${(_limit / 60).floor().toString()} min. limit"),
-                ),
-              ],
-            ),
-          )
         ],
       ),
       floatingActionButton: Padding(
         padding: EdgeInsets.all(8),
-        child: FloatingActionButton(
-          foregroundColor: black,
-          backgroundColor: Colors.white,
-          child: _watch.isRunning
-              ? Text("Pause")
-              : _op == 1 ? Text("Reset") : Text("Go"),
-          onPressed: () {
-            setState(() {
-              if (_watch.isRunning) {
-                _watch.stop();
-              } else {
-                if (_op == 1) {
-                  reset();
-                } else {
-                  _watch.start();
-                  Timer.periodic(Duration(milliseconds: 950), update);
-                }
-              }
-            });
-          },
-        ),
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              hasStarted
+                  ? Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FloatingActionButton(
+                        foregroundColor: black,
+                        backgroundColor: Colors.white,
+                        child: Text("Reset"),
+                        onPressed: () {
+                          reset();
+                        },
+                      ),
+                    )
+                  : Container(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: FloatingActionButton(
+                  foregroundColor: black,
+                  backgroundColor: Colors.white,
+                  child: Text(_actionLabel),
+                  onPressed: () {
+                    setState(() {
+                      switch (_state) {
+                        case TimerState.init:
+                          start();
+                          break;
+                        case TimerState.play:
+                          pause();
+                          break;
+                        case TimerState.pause:
+                          resume();
+                          break;
+                        case TimerState.end:
+                          end();
+                          break;
+                        case TimerState.reset:
+                          reset();
+                          break;
+                      }
+                    });
+                  },
+                ),
+              ),
+            ]),
       ),
     );
   }
